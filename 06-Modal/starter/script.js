f8c53dd27e892abdce1a14bcbac3e571079dce91
guilherme escarduca <guilherme.escarduca@celfocus.com>
2022-06-03 12:50:46 +0100

    OfficeVersion-Modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+//we need the query selector all to get all elements otherwise
+//we would just get the first one
+//this creates a node list of the modals
+const btnOpenModal= document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+
+for(let i = 0; i<btnOpenModal.length;i++)
+    btnOpenModal[i].addEventListener('click', function(){
+        console.log('Button Clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+        
+    });
+
