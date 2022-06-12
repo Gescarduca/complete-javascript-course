096f69e4b214a303c1cdb91e914dbd0a4808e125
Gescarduca <44589787+Gescarduca@users.noreply.github.com>
2022-06-12 16:18:31 +0100

    Commit before pig game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,10 +13,16 @@ console.log(document.querySelector('.guess').value);
 
 //listening to events
 
-const number = Math.trunc(Math.random() * 20) + 1;
-let scoreNumber = Number(document.querySelector('.score').textContent);
+let number = Math.trunc(Math.random() * 20) + 1;
+let scoreNumber = 20;
+let highscore = 0;
+console.log(number);
 
-document.querySelector('.number').textContent = number;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+//document.querySelector('.number').textContent = number;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -25,27 +31,59 @@ document.querySelector('.check').addEventListener('click', function () {
   //0 is falsey value
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number !';
-  }
-    else if (guess === number) {
-      //document.querySelector('.number').textContent = number;
-      document.querySelector('.message').textContent = 'Correct Number!';
-      document.querySelector('body').style.backgroundColor = '#60b347';
-      document.querySelector('.number').style.width = '30rem';
-      increaseScore(scoreNumber);
-    } else if (guess > number) {
-      document.querySelector('.message').textContent = 'Too High!';
-      decreaseScore(scoreNumber);
-    } else if (guess < number) {
-      document.querySelector('.message').textContent = 'Too Low!';
-      decreaseScore(scoreNumber);
+  } else if (guess === number) {
+    //document.querySelector('.number').textContent = number;
+    //document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    scoreNumber++;
+    document.querySelector('.score').textContent = scoreNumber;
+
+    if (scoreNumber > highscore) {
+      document.querySelector('.highscore').textContent = scoreNumber;
     }
-   else {
+  } else if (guess !== number) {
+    if (scoreNumber > 1) {
+      displayMessage(guess > number ? 'Too High!' : 'Too Low!');
+      scoreNumber--;
+      document.querySelector('.score').textContent = scoreNumber;
+      //decreaseScore(scoreNumber);
+    }
+  } else {
+    displayMessage('You lost the game');
+    document.querySelector('.score').textContent = 0;
+  }
+
+  /*   else if (guess > number) {
+    //document.querySelector('.message').textContent = 'Too High!';
+    displayMessage('Too High!');
+    decreaseScore(scoreNumber);
+  } else if (guess < number) {
+    //document.querySelector('.message').textContent = 'Too Low!';
+    displayMessage('Too Low!');
+    decreaseScore(scoreNumber);
+  } else {
     document.querySelector('.message').textContent = 'You Lost the Game!';
   }
+} */
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  number = Math.trunc(Math.random() * 20) + 1;
+  console.log(number + 'In again button');
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').value = '?';
+  document.querySelector('.guess').value = '';
+  console.log(number);
+  displayMessage('Start guessing...');
+  document.querySelector('.check').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
 
 function decreaseScore(scoreNumber) {
-  console.log(scoreNumber);
+  console.log(scoreNumber + 'In decrease score');
   scoreNumber--;
   document.querySelector('.score').textContent = scoreNumber;
 }
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,18 +2,52 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModel = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 //we need the query selector all to get all elements otherwise
 //we would just get the first one
 //this creates a node list of the modals
-const btnOpenModal= document.querySelectorAll('.show-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
 
-for(let i = 0; i<btnOpenModal.length;i++)
-    btnOpenModal[i].addEventListener('click', function(){
-        console.log('Button Clicked');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-        
-    });
+//classes allows us to work with multiple css elements
 
+const openModal = function () {
+  //allows you to show hidden elements
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+/* for (let i = 0; i < btnOpenModal.length; i++)
+  btnCloseModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }); */
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//add escape key event to close the modal
+//keyboard events are called global events since
+//they happen to the whole document
+//four types of keyboard events
+//keypress- activates continuously as finger is kept on the key
+//keydown-- activates on down travel
+//key up -- activates on release the key
+
+//for this case we need to pass the event as an argument
+//so that we can check which key event ocurred
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
